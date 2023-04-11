adaabdc5db0fb50eb5122abd72c4b4eb3d1ac550
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-11 16:23:35 +0100

    fix task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,6 +2,6 @@
 CREATE VIEW need_meeting AS
     SELECT
         name
-    FROM need_meeting
+    FROM students
     WHERE score < 80
     AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH))
