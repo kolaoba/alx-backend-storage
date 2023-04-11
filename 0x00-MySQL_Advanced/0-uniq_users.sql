187a4d38505a4c4aa086a80990a3b7d311188c25
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-11 08:01:18 +0100

    fix task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- id: INT, not null, auto increment, primary key
 -- email: VARCHAR(255), not null
 -- name: VARCHAR(255)
-CREATE TABLE IF NOT EXISTS holberton.users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
