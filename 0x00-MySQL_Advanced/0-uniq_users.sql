1657df0780d7078eb497205b14e01538e31c12ce
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-11 07:59:58 +0100

    add comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- Create a table users with the following attributes:
+-- id: INT, not null, auto increment, primary key
+-- email: VARCHAR(255), not null
+-- name: VARCHAR(255)
 CREATE TABLE IF NOT EXISTS holberton.users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
