14c827320be9345cc13372a12321eb459cb563ad
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-12 12:10:10 +0100

    add task 0 and 1

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Writing strings to Redis
+"""
+
+import redis
+from typing import Union, Callable, Optional
+
+from uuid import uuid4
+
+
+class Cache:
+    """
+    defines Cache class
+    """
+
+    def __init__(self) -> None:
+        """
+        initialize Cache instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        method to store input data using generated uuid key
+
+        args:
+            data (str | bytes | int | float): data to be stored
+
+        returns:
+            key (str): uuid4 g
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str,
+                                                    bytes,
+                                                    int,
+                                                    float,
+                                                    None]:
+        """
+        method to retrieve stored data with optional conversion function
+
+        args:
+            key (str):
+        """
+        if not self._redis.get(key):
+            return None
+
+        if fn is None:
+            return self._redis.get(key)
+
+        return fn(self._redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """
+        method to retrieve stored value in str format
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        method to retrieve stored value in int format
+        """
+        return self.get(key, int)
