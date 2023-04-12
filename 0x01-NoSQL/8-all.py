4085ffc49790e35a191c5089971f4369ab0d67b8
Kolapo Obajulwa <Kolaobajulwa@gmail.com>
2023-04-12 02:35:53 +0100

    add task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+List all documents in Python
+"""
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection
+    """
+    if mongo_collection is not None:
+        return mongo_collection.find()
+    return []
